7c0426a6177277b08a9b77824c1f47dc4c3d30d2
akazad12 <akazad12@aim.com>
2025-12-04 02:07:12 -0500

    Errors in Judgment completed, comments added

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 //Part 1 Growing Pains
-let radius =5;
+let radius = 5;
 const Pi = 3.1415;
-const gardenArea = Pi*radius*radius;
+const gardenArea = Pi * radius * radius;
 
 const plantMinSpace = .8;
 let numPlant = 20;
 
-let week =3;    //MODIFY THIS VALUE
+let week = 3;    //MODIFY THIS VALUE
 
-let multiplier = 2**(week-1)
-let totalPlants = multiplier*numPlant*plantMinSpace
+let multiplier = 2 ** (week - 1)
+let totalPlants = multiplier * numPlant * plantMinSpace
 
 
 let action;
 
+try {
+    //Part 3 Errors in Judgment
+    if (numPlant * plantMinSpace > gardenArea) {        
+        throw new Error('Starting off with too many plants');
+    }
 
-if (totalPlants>(.8*gardenArea)){
+    if (totalPlants > (.8 * gardenArea)) {
 
-    action = 'Pruned';
+        action = 'Pruned';
 
-} else if (totalPlants>(.5*gardenArea)){
+    } else if (totalPlants > (.5 * gardenArea)) {
 
-    action = 'Monitored';
+        action = 'Monitored';
 
-} else if (totalPlants<(.5*gardenArea)){
-    action = 'Planted';
+    } else if (totalPlants < (.5 * gardenArea)) {
+        action = 'Planted';
+    }
+
+    output1 = `for week ${week} the plants should be ${action}\n`;
+}
+catch (error) {
+
+    console.error('There is and issue: ',error.message);
+
 }
 
-output1 = `for week ${week} the plants should be ${action}\n`;
-
-
-// console.log(output1);
+console.log(output1);
 // OUTPUT --------------------------------
 // for week 1 the plants should be Planted
 // for week 2 the plants should be Planted
@@ -38,28 +48,24 @@ output1 = `for week ${week} the plants should be ${action}\n`;
 
 
 
-
-
-
 //Part 2 Thinking Bigger
 let numStarting = 100;
 let noPruneWeeks = 10
-let numEnding = (2**(noPruneWeeks-1))*numStarting;
-let spaceNeeded = numEnding*plantMinSpace;
-let spacediffernce = spaceNeeded-gardenArea;
+let numEnding = (2 ** (noPruneWeeks - 1)) * numStarting;
+let spaceNeeded = numEnding * plantMinSpace;
+let spacediffernce = spaceNeeded - gardenArea;
 
 console.log(`Amount of additional space required: ${spacediffernce}`);
 
-let expandedRadius = (spaceNeeded/Pi)**.5
+//Output--------------------------
+//  Amount of additional space required: 40881.4625
+
+let expandedRadius = (spaceNeeded / Pi) ** .5
 
 console.log(`Radius of Expanded Garden: ${expandedRadius}`);
 
+//Output--------------------------
+// Radius of Expanded Garden: 114.1856272590599
 
 
 
-
-
-
-
-
-//Part 3 Errors in Judgment
